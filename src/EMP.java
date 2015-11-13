3377ea9bf72a4a46912b4596f3d0a911cacd90ec
Михаил Ионкин <ionkin-rus@mail.ru>
2015-11-13 20:51:33 +0400

    EMPh

---
+++ src/EMP.java
@@ -0,0 +1,14 @@
+/**
+ * Created by misha on 11.11.15.
+ */
+import solution.Data;
+import solution.Data.*;
+import window.Graph;
+
+public class EMP {
+    public static void main(String[] args){
+        //Data.m();
+        Graph g = new Graph();
+        g.setVisible(true);
+    }
+}
