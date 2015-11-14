8697b64090282703674465bc9081f84beb0d6ee6
Mikhail42 <ionkin-rus@mail.ru>
2015-11-14 11:22:05 +0400

    Update Data.scala

--- src/solution/Data.scala
+++ src/solution/Data.scala
@@ -1,11 +1,13 @@
 package solution
 object Data{
-  var c: Double = 2.0
-  var K: Double = 0.01
-  var R: Double = 2.0
-  var beta: Double = 0.1
-  // @param eps: accuracy: $\abs{u-u^*}, u^* = \sum_{0}^{N} \{\dots\}$
-  var eps: Double = 1e-7
+  var c     = 2.0
+  var K     = 0.01
+  var R     = 2.0
+  var beta  = 0.1
+  // eps: accuracy: $\abs{u-u^*}, u^* = \sum_{0}^{N} \{\dots\}$
+  var eps   = 1e-7
+  // time
+  var t     = 1e-3
 
   def R2 = R*R
   def kDelcR2 = K/(c*R2)
@@ -15,18 +17,16 @@ object Data{
   val maxN0 = 40000
   /** counts of $\{f_{2k}\}$, $\{P_{2k}(0)\}$ */
   /** $n_0 = \Bigg\lceil \sqrt{\frac{\beta R^2}{16K\varepsilon}}\Bigg\rceil$*/
-  def n0 =  math.min(math.sqrt(bR2DelK/(16*eps)), (maxN0>>1)).toInt
+  def n0 =  math.min(math.sqrt(bR2DelK/(16.0*eps)), (maxN0>>1)).toInt
 
   /** $P_{2n}(0) = - P_{2n-2}(0) \frac{2n-1}{2n},\; P_{0}(0)=1$ */
-  val P2k = new Array[Double](n0)
+  val P2k = new Array[Double](maxN0)
   def setAllP2k{
-    P2k(0)=1
-    for (i <- 1 until n0)
+    P2k(0)=1.0
+    for (i <- 1 until maxN0)
       P2k(i) = -P2k(i-1)*((i << 1)-1)/(i << 1)
   }
 
-  // time
-  var t: Double = 1e-3
   // initial of $\{P_{2k}\}$
   def init = setAllP2k
 }
